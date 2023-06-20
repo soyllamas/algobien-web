6cf7bce3361343aa2cbeddb3e7a68558e85b8249
Daniel Llamas <daniel@soyllamas.com>
2023-06-20 07:58:57 -0600

    feat: simple landing + firebase hosting + github actions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,19 @@
-<h1 class="text-center text-4xl my-12">Recomendaciones — Comidas & Bebidas</h1>
-<div class="h-0.5 mx-10 w-full bg-gray-200 mb-12"></div>
+<svelte:head>
+    <title>Recomendaciones — Comidas & Bebidas</title>
+</svelte:head>
+<div class="px-12 w-full mb-16 mt-32">
+    <div class="h-0.5 w-full bg-gray-200"></div>
+</div>
 <div class="max-w-2xl mx-auto">
     <h2 class="text-5xl">Una colección de recomendaciones que quieres tener en el radar.</h2>
     <p class="text-gray-400 mt-4">Una cuidada selección de restaurantes, bares, cafeterías y otros conceptos de los que
         vas a necesitar saber más.</p>
 </div>
-<div class="h-0.5 mx-10 w-full bg-gray-200 mt-12"></div>
+<div class="px-12 w-full mt-16">
+    <div class="h-0.5 w-full bg-gray-200"></div>
+</div>
 
 <style lang="postcss">
-    h1 {
-        font-family: 'Instrument Serif', serif;
-    }
-
     h2 {
         font-family: 'Instrument Serif', serif;
     }
