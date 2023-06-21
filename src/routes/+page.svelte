67946f926ab01bc80967d697580f3af80c60af71
Daniel Llamas <daniel@soyllamas.com>
2023-06-20 21:39:26 -0600

    fix: text padding

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 <div class="px-12 w-full mb-16 mt-32">
     <div class="h-0.5 w-full bg-gray-200"></div>
 </div>
-<div class="max-w-2xl mx-auto">
+<div class="max-w-2xl mx-auto px-5">
     <h2 class="text-5xl">Una colección de recomendaciones que quieres tener en el radar.</h2>
     <p class="text-gray-400 mt-4">Una cuidada selección de restaurantes, bares, cafeterías y otros conceptos de los que
         vas a necesitar saber más.</p>
