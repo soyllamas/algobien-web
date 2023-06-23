901ebb42705326b3d30ecadf3f66f12fc7366dec
Daniel Llamas <daniel@soyllamas.com>
2023-06-22 18:31:34 -0600

    feat: beautify the landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,25 @@
 <svelte:head>
-    <title>Recomendaciones — Comidas & Bebidas</title>
+    <title>Algo Bien — Comidas & Bebidas</title>
 </svelte:head>
-<div class="px-12 w-full mb-16 mt-32">
+<h1 class="text-8xl text-center mt-12">Algo Bien.</h1>
+<div class="px-12 w-full mb-12 mt-16">
     <div class="h-0.5 w-full bg-gray-200"></div>
 </div>
-<div class="max-w-2xl mx-auto px-5">
-    <h2 class="text-5xl">Una colección de recomendaciones que quieres tener en el radar.</h2>
-    <p class="text-gray-400 mt-4">Una cuidada selección de restaurantes, bares, cafeterías y otros conceptos de los que
-        vas a necesitar saber más.</p>
+<div class="max-w-5xl md:mx-auto px-5 md:px-12 grid grid-cols-1 md:grid-cols-5 gap-x-0 md:gap-x-8 items-center">
+    <div class="col-span-2 aspect-video bg-cover bg-center rounded-2xl mb-6 md:mb-0"
+         style="background-image: url('cover.jpg')"></div>
+    <div class="col-span-3">
+        <h2 class="text-5xl">Una colección de recomendaciones que quieres tener en el radar.</h2>
+        <p class="text-gray-400 mt-4">Explora una cuidada selección de restaurantes, bares, cafeterías y otros conceptos
+            de los que vas a necesitar saber más.</p>
+    </div>
 </div>
-<div class="px-12 w-full mt-16">
+<div class="px-12 w-full mt-12">
     <div class="h-0.5 w-full bg-gray-200"></div>
 </div>
 
 <style lang="postcss">
-    h2 {
+    h1, h2 {
         font-family: 'Instrument Serif', serif;
     }
 </style>
