7aa62efa7531479fb34d6095e63f81bd26f825c7
Daniel Llamas <daniel@soyllamas.com>
2023-07-14 07:13:24 -0600

    fix: minor visual details

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,14 +2,14 @@
     <title>10/10 — Comidas & Bebidas</title>
 </svelte:head>
 <nav class="border-b-2 border-b-black fixed w-full bg-gray-50 h-16 grid place-items-center">
-    <h1 class="text-center bg-black text-white px-2">10/10</h1>
+    <h1 class="text-center bg-black border-gray-300 border-b-2 border-r-2 text-white px-2">10/10</h1>
 </nav>
 <div class="max-w-3xl mx-auto pt-28 md:pt-32 px-4">
-    <h2 class="text-2xl md:text-4xl">Una colección de recomendaciones que quieres tener en el radar.</h2>
+    <h2 class="text-2xl font-medium md:text-4xl">Una colección de recomendaciones que quieres tener en el radar.</h2>
     <p class="md:text-lg text-gray-400 mt-4">Explora una cuidada selección de restaurantes, bares, cafeterías y
         otros conceptos de los que vas a necesitar saber más.</p>
 </div>
-<div class="grid md:grid-cols-2 gap-4 mx-auto max-w-3xl px-4 py-12">
+<div class="grid md:grid-cols-2 gap-4 mx-auto max-w-4xl px-4 py-12">
     <img src="https://firebasestorage.googleapis.com/v0/b/soy-llamas.appspot.com/o/destinations%2Fpuerto-escondido%2FPE_1.jpg?alt=media&token=9915c7c5-3c0b-43b5-8f39-398e0884ce42"
          alt="puerto escondido">
     <img src="https://firebasestorage.googleapis.com/v0/b/soy-llamas.appspot.com/o/destinations%2Fpuerto-escondido%2FPE_2.jpg?alt=media&token=ccf0b33a-4f03-4fe6-a8de-b48a64da2810"
