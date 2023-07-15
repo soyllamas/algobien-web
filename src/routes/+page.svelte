a6efa7e9e4cf4ef204bb7638e0931fd27119338b
Daniel Llamas <daniel@soyllamas.com>
2023-07-15 12:59:34 -0600

    feat: new layout + destinations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,28 @@
 <svelte:head>
     <title>10/10 — Comidas & Bebidas</title>
 </svelte:head>
-<nav class="border-b-2 border-b-black fixed w-full bg-gray-50 h-16 grid place-items-center">
-    <h1 class="text-center bg-black border-gray-300 border-b-2 border-r-2 text-white px-2">10/10</h1>
+<nav class="border-b-2 border-b-gray-200 fixed w-full bg-white h-16 grid place-items-center z-50">
+    <h1 class="text-center bg-black font-medium text-white px-2">10/10</h1>
 </nav>
-<div class="max-w-3xl mx-auto pt-28 md:pt-32 px-4">
+<div class="h-16"></div>
+<div class="md:mx-auto max-w-3xl px-4 py-12">
     <h2 class="text-2xl font-medium md:text-4xl">Una colección de recomendaciones que quieres tener en el radar.</h2>
     <p class="md:text-lg text-gray-400 mt-4">Explora una cuidada selección de restaurantes, bares, cafeterías y
         otros conceptos de los que vas a necesitar saber más.</p>
 </div>
-<div class="grid md:grid-cols-2 gap-4 mx-auto max-w-4xl px-4 py-12">
-    <img src="https://firebasestorage.googleapis.com/v0/b/soy-llamas.appspot.com/o/destinations%2Fpuerto-escondido%2FPE_1.jpg?alt=media&token=9915c7c5-3c0b-43b5-8f39-398e0884ce42"
-         alt="puerto escondido">
-    <img src="https://firebasestorage.googleapis.com/v0/b/soy-llamas.appspot.com/o/destinations%2Fpuerto-escondido%2FPE_2.jpg?alt=media&token=ccf0b33a-4f03-4fe6-a8de-b48a64da2810"
-         alt="puerto escondido">
-    <img src="https://firebasestorage.googleapis.com/v0/b/soy-llamas.appspot.com/o/destinations%2Fpuerto-escondido%2FPE_3.jpg?alt=media&token=829af8cb-cc6e-4592-b0be-90179af09be8"
-         alt="puerto escondido">
-    <img src="https://firebasestorage.googleapis.com/v0/b/soy-llamas.appspot.com/o/destinations%2Fpuerto-escondido%2FPE_4.jpg?alt=media&token=7ff30c5f-3d41-451d-86de-f2f1ff5913cb"
-         alt="puerto escondido">
-    <img src="https://firebasestorage.googleapis.com/v0/b/soy-llamas.appspot.com/o/destinations%2Fpuerto-escondido%2FPE_5.jpg?alt=media&token=9a4515cf-ca3d-4ddf-b623-a76f7a586466"
-         alt="puerto escondido">
-    <img src="https://firebasestorage.googleapis.com/v0/b/soy-llamas.appspot.com/o/destinations%2Fpuerto-escondido%2FPE_6.jpg?alt=media&token=e1d2c186-82c4-4095-9dba-4ce5353ccaa3"
-         alt="puerto escondido">
+<div class="grid md:grid-cols-3 gap-4 mx-auto max-w-5xl px-4 pb-4">
+    <div class="destination bg-[url('https://firebasestorage.googleapis.com/v0/b/soy-llamas.appspot.com/o/destinations%2Fpuerto-escondido%2FPE_5.jpg?alt=media&token=9a4515cf-ca3d-4ddf-b623-a76f7a586466')]">
+        <p class="destination-title">Puerto Escondido</p>
+    </div>
+    <div class="destination bg-[url('/los-cabos.jpg')]">
+        <p class="destination-title">Los Cabos</p>
+    </div>
+    <div class="destination bg-[url('/qro.jpg')]">
+        <p class="destination-title">Querétaro</p>
+    </div>
+    <div class="destination bg-[url('/valle.jpg')]">
+        <p class="destination-title">Valle de Bravo</p>
+    </div>
 </div>
 
 <style lang="postcss">
@@ -33,7 +34,11 @@
         font-family: 'IBM Plex Sans', sans-serif;
     }
 
-    img {
-        @apply rounded-xl md:rounded-2xl
+    .destination {
+        @apply rounded-xl h-[80vh] bg-cover relative bg-center
+    }
+
+    .destination-title {
+        @apply absolute bottom-0 left-0 rounded-xl text-white font-semibold px-6 py-8 text-5xl bg-gradient-to-t from-[#00000080] to-[#00000000] w-full
     }
 </style>
